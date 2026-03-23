fbbd78dc8e0bcd5d3016016ee2772885bcdcabd1
Diego <diego.gutierrez@chakray.com>
2026-03-23 17:24:15 -0600

    Feature: dockerfile and image

---
+++ src/test/java/com/devdiego/springboot_web/product/infraestructure/api/database/ProductRepositoryImplTest.java
@@ -0,0 +1,15 @@
+package com.devdiego.springboot_web.product.infraestructure.api.database;
+
+import com.devdiego.springboot_web.product.infraestructure.api.ProductJpaRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class ProductRepositoryImplTest {
+    ProductJpaRepository productRepository = Mockito.mock(ProductJpaRepository.class);
+    ProductRepositoryImpl productRepositoryImpl = new ProductRepositoryImpl(productRepository);
+
+    @Test
+    public void upsertTest() {
+
+    }
+}
